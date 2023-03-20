7a06c01c14e1fd0f103e7fc183b506da1c445d31
diamond9159 <johnford9159@gmail.com>
2023-03-20 07:49:02 -0700

    account page

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,11 +1,12 @@
-import React from "react"
+import React from "react";
 
 export function Container(props: any) {
   return (
     <div
-      className={`w-full px-5 py-10 md:px-10 md:py-10 lg:px-30 lg:py-15 ${
+      className={`w-full px-5 py-10 md:px-20 md:py-10 lg:px-30 lg:py-15 ${
         props.className ? props.className : ""
-      }`}>
+      }`}
+    >
       {props.children}
     </div>
   );
--- src/components/user/account/AccountPage.tsx
+++ src/components/user/account/AccountPage.tsx
@@ -1,25 +1,11 @@
 import { useTranslations } from "next-intl";
-import { FC } from "react";
+import { FC, useState } from "react";
 import Image from "next/image";
 
-import { Wrapper, Container, Input, Button } from "../../ui";
+import { Wrapper, Container, Button, Tab } from "../../ui";
 
-type Props = {
-  label: string;
-};
-
-const Tab: FC<Props> = (props) => {
-  const { label } = props;
-  return (
-    <>
-      <button className="w-fit border-b-2 border-gray-100 hover:border-gray-600 text-lg text-600 px-3 py-2">
-        {label}
-      </button>
-    </>
-  );
-};
-
-const Profile = () => {
+const ProfileSection = () => {
+  const t = useTranslations("user.account");
   return (
     <Container className="flex flex-col md:flex-row gap-20">
       <div>
@@ -29,7 +15,7 @@ const Profile = () => {
         </div>
         <span className="text-gray-600 ">
           Tip: Tip: Choose a picture where people can see your face.
-        </span>{" "}
+        </span>
         <br></br>
         <span className="text-gray-600 ">.JPG or .PNG. Max. 20 MB</span>
       </div>
@@ -66,69 +52,80 @@ const Profile = () => {
   );
 };
 
+const AccountSection = () => {
+  const t = useTranslations("user.account");
+  return (
+    <Container className="flex flex-col gap-20">
+      <div>
+        <h2 className="text-start">{t("account-settings")}</h2>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3>Your Email Address</h3>
+        <input
+          type="email"
+          name="username"
+          placeholder="Enter your Email"
+          required
+        />
+        <span>Your email address is verified</span>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3>Phone Number</h3>
+        <input
+          className="w-[200px]"
+          type="text"
+          name="phonenumber"
+          placeholder="Enter your Phone number"
+          required
+        />
+        <span className="text-gray-600">
+          artisanNow is built on relationships. Help other people get to
+          know you.
+        </span>
+
+      </div>
+
+      {/* Password */}
+      <div className="flex flex-col gap-2">
+        <h3>New Password</h3>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your new Password"
+          required
+        />
+        <span>Please confirm the new password again.</span>
+      </div>
+      <Button className="w-[50%]" label={t("save-profile")} />
+
+      {/* Account */}
+      <div className="flex flex-col gap-2">
+        <h3>Delete Account</h3>
+
+        <span>Your account will be deleted within 48 hours.</span>
+        <span>If you want to delete it please click on the button below.</span>
+      </div>
+      <Button className="w-[50%]" label={"Delete Account"} />
+    </Container>
+  )
+
+}
+
 export function AccountPage() {
   const t = useTranslations("user.account");
+
+  const [isProfile, setProfile] = useState<boolean>(true)
 
   return (
     <>
       <Wrapper className="w-full flex justify-center">
         <div className="w-[90%] lg:w-[70%]">
           <Container className="flex gap-x-4 items-center justify-end">
-            <Tab label={t("profile-settings")} />
-            <Tab label={t("account-settings")} />
+            <Tab label={t("profile-settings")} action={() => setProfile(true)} isSelected={isProfile} />
+            <Tab label={t("account-settings")} action={() => setProfile(false)} isSelected={!isProfile} />
           </Container>
-          <Container className="flex flex-col gap-20">
-            <div>
-              <h2 className="text-start">{t("profile-settings")}</h2>
-            </div>
-            <div className="flex flex-col gap-2">
-              <h3>Your Email Address</h3>
-              <input
-                type="email"
-                name="username"
-                placeholder="Enter your Email"
-                required
-              />
-              <span>Your email address is verified</span>
-            </div>
-            <div className="flex flex-col gap-2">
-              <h3>Phone Number</h3>
-              <input
-                className="w-[200px]"
-                type="text"
-                name="phonenumber"
-                placeholder="Enter your Phone number"
-                required
-              />
-              <span className="text-gray-600">
-                artisanNow is built on relationships. Help other people get to
-                know you.
-              </span>
-             
-            </div>
-            
-            {/* Password */}
-            <div className="flex flex-col gap-2">
-              <h3>New Password</h3>
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter your new Password"
-                required
-              />
-              <span>Please confirm the new password again.</span>
-            </div>
-            <Button className="w-[50%]" label={t("save-profile")} />
-
-            {/* Account */}
-            <div className="flex flex-col gap-2">
-              <h3>Delete Account</h3>
-              
-              <span>Your account will be deleted within 48 hours.</span> 
-              <span>If you want to delete it please click on the button below.</span>
-            </div>
-            <Button className="w-[50%]" label={"Delete Account"} />
-          </Container>
+          {isProfile && <ProfileSection />}
+          {!isProfile && <AccountSection />}
         </div>
       </Wrapper>
     </>
